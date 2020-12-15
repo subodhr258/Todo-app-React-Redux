7b33a1bf2a08544236ee503d119bf46407223ad7
Subodh Rajpopat <subodhr2001@gmail.com>
2020-12-15 14:03:40 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App m-5">
+      <TodoInput />
+      <TodoList />
+
+    </div>
+  );
+}
+
+export default App;
